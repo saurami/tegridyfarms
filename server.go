e4c2ecd0cba4efc1ea226cc975a82355f8f5799c
Saurabh Mishra <saurabhm@proton.me>
2022-10-27 21:39:51 -0700

    added health handler for returning JSON

--- server.go
+++ server.go
@@ -2,14 +2,24 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
 )
 
-func contextRoot(w http.ResponseWriter, r *http.Request) {
+func helloHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, World!")
 }
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    w.Write([]byte(`{"server": "running", "status": "operational"}`))
+}
+
 func main() {
-    http.HandleFunc("/", contextRoot)
-    http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/hello", helloHandler)
+    http.HandleFunc("/health", healthHandler)
+    err := http.ListenAndServe("localhost:8080", nil)
+    if err != nil {
+        log.Fatalf("Unable to start HTTP server: %v", err)
+    }
 }
