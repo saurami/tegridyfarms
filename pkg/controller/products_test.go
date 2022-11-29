34021f2b01cc71c473a4ec23a051aa982a80eebc
Saurabh Mishra <saurabhm@proton.me>
2022-11-28 16:54:21 -0800

    test response for valid product

--- pkg/controller/products_test.go
+++ pkg/controller/products_test.go
@@ -42,3 +42,40 @@ func TestGetProducts(t *testing.T) {
         }
     })
 }
+
+
+func TestRetrieveExistingProduct(t *testing.T) {
+    request, err := http.NewRequest("GET", "/products/Tegridy%20Weed", nil)
+    if err != nil {
+        t.Fatalf("Unable to reach endpoint: %v", err)
+    }
+
+    response := httptest.NewRecorder()
+
+    handler := http.HandlerFunc(RetrieveProduct)
+    handler.ServeHTTP(response, request)
+
+    t.Run("Response Code", func(t *testing.T) {
+        got := response.Code
+        want := http.StatusOK
+        if got != want {
+            t.Errorf("Incorrect response header: got %v; want %v", got, want)
+        }
+    })
+
+    t.Run("Header", func(t *testing.T) {
+        got := response.Header().Get("Content-Type")
+        want := "application/json"
+        if got != want {
+            t.Errorf("Unexpected content type: got %v; want %v", got, want)
+        }
+    })
+
+    t.Run("Content", func(t *testing.T){
+        var singleProduct model.Product
+        err := json.NewDecoder(response.Body).Decode(&singleProduct)
+        if err != nil {
+            t.Errorf("Error parsing response data into product structure: %v", err)
+        }
+    })
+}
