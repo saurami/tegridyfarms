2d59b8030901e48afb741c96cd03fc5fd6f12b21
Saurabh Mishra <saurabhm@proton.me>
2022-11-28 16:54:01 -0800

    return response for single product

--- pkg/controller/products.go
+++ pkg/controller/products.go
@@ -3,6 +3,7 @@ package controller
 import (
     "net/http"
     "encoding/json"
+    "strings"
     "github.com/saurabh/tegridyfarms/pkg/model"
 )
 
@@ -14,5 +15,21 @@ func GetProducts(w http.ResponseWriter, r *http.Request) {
     } else {
         w.Header().Set("Content-Type", "application/json")
         json.NewEncoder(w).Encode(model.Products)
+    }
+}
+
+func RetrieveProduct(w http.ResponseWriter, r *http.Request) {
+    productName := strings.TrimPrefix(r.URL.Path, "/products/")
+    if productName == "" {
+        http.Error(w, "Invalid product name", http.StatusNotFound)
+    } else if r.Method != "GET" {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    } else {
+        w.Header().Set("Content-Type", "application/json")
+        for _, product := range model.Products {
+            if product.Name == productName {
+                json.NewEncoder(w).Encode(product)
+            }
+        }
     }
 }
