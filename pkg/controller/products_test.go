4263cdb76448630fb370fe0d2505f99a68ac213e
Saurabh Mishra <saurabh.m@mailfence.com>
2022-11-29 23:29:29 -0800

    added test for verifying response data contents

--- pkg/controller/products_test.go
+++ pkg/controller/products_test.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/saurabh/tegridyfarms/pkg/model"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -77,10 +79,54 @@ func TestRetrieveExistingProduct(t *testing.T) {
 		if err != nil {
 			t.Errorf("Error parsing response data into product structure: %v", err)
 		}
+
+		if singleProduct.Name != "Tegridy Weed" && !strings.Contains(singleProduct.Updated, "2022") {
+			t.Error("Response data does not have product information")
+		}
 	})
+
 }
 
 func TestCreateProduct(t *testing.T) {
+	hemp := model.Product{
+		Name:    "Hemp Cookies",
+		Updated: "2022-14-23 12:12:12",
+	}
+
+	hempJSON, err := json.Marshal(hemp)
+	if err != nil {
+		t.Errorf("New product is different from the defined structure: %v", err)
+	}
+
+	request, err := http.NewRequest("POST", "/product", bytes.NewBuffer(hempJSON))
+	if err != nil {
+		t.Errorf("Error opening file: %v", err)
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	response := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(CreateProduct)
+	handler.ServeHTTP(response, request)
+
+	t.Run("Response Code", func(t *testing.T) {
+		got := response.Code
+		want := http.StatusOK
+		if got != want {
+			t.Errorf("Unexpected response code ... got %v; want %v", got, want)
+		}
+	})
+
+	t.Run("Header", func(t *testing.T) {
+		got := response.Header().Get("Content-Type")
+		want := "application/json"
+		if got != want {
+			t.Errorf("Invalid response header ... got %v; want %v", got, want)
+		}
+	})
+}
+
+func TestCreateProductBatch(t *testing.T) {
 	hempFile, err := os.Open("../../config/hemp_products.json")
 	if err != nil {
 		t.Errorf("Error opening file: %v", err)
