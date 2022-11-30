73954c68d9d076988f0387dad3fcd1c51cdb87f7
Saurabh Mishra <saurabh.m@mailfence.com>
2022-11-30 01:40:01 -0800

    incorporated routes for GET and DELETE product

--- cmd/main/tegridy.go
+++ cmd/main/tegridy.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/saurabh/tegridyfarms/pkg/controller"
+	"github.com/saurabh/tegridyfarms/pkg/controller"
+	"github.com/saurabh/tegridyfarms/pkg/routes"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.HandleFunc("/home", controller.HomeHandler)
-    http.HandleFunc("/hello", controller.HelloHandler)
-    http.HandleFunc("/health", controller.HealthHandler)
-    http.HandleFunc("/outdoor", controller.OutdoorHandler)
-    http.HandleFunc("/products", controller.GetProducts)
-    http.HandleFunc("/products/", controller.RetrieveProduct)
-    http.HandleFunc("/product", controller.CreateProduct)
-    
-    err := http.ListenAndServe("127.0.0.1:8080", nil)
-    if err != nil {
-        log.Fatalf("Unable to start HTTP server: %v", err)
-    }
+	http.HandleFunc("/home", controller.HomeHandler)
+	http.HandleFunc("/hello", controller.HelloHandler)
+	http.HandleFunc("/health", controller.HealthHandler)
+	http.HandleFunc("/outdoor", controller.OutdoorHandler)
+	http.HandleFunc("/products", controller.GetProducts)
+	http.HandleFunc("/products/", routes.RegisteredPaths)
+	http.HandleFunc("/product", controller.CreateProduct)
+
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
+	if err != nil {
+		log.Fatalf("Unable to start HTTP server: %v", err)
+	}
 }
