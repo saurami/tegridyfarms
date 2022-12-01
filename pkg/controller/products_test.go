e9d457ac0187202a6a1565618c3f6ac0ea13c95b
Saurabh Mishra <saurabh.m@mailfence.com>
2022-11-30 18:48:41 -0800

    added tests for PUT and DELETE operations

--- pkg/controller/products_test.go
+++ pkg/controller/products_test.go
@@ -101,7 +101,7 @@ func TestCreateProduct(t *testing.T) {
 
 	request, err := http.NewRequest("POST", "/product", bytes.NewBuffer(hempJSON))
 	if err != nil {
-		t.Errorf("Error opening file: %v", err)
+		t.Errorf("Error performing request: %v", err)
 	}
 
 	request.Header.Set("Content-Type", "application/json")
@@ -127,8 +127,8 @@ func TestCreateProduct(t *testing.T) {
 	})
 }
 
-func TestCreateProductBatch(t *testing.T) {
-	hempFile, err := os.Open("../../config/hemp_products.json")
+func TestCreateProductFromFile(t *testing.T) {
+	hempFile, err := os.Open("../../config/hemp_product.json")
 	if err != nil {
 		t.Errorf("Error opening file: %v", err)
 	}
@@ -168,4 +168,90 @@ func TestCreateProductBatch(t *testing.T) {
 			t.Errorf("Error parsing response data into the defined structure: %v", err)
 		}
 	})
+}
+
+func TestUpdateExistingProduct(t *testing.T) {
+	weed := model.Product{
+		Name:    "Tegridy Jungle Bud",
+		Updated: "2022-22-22 00:12:01",
+	}
+
+	weedJSON, err := json.Marshal(weed)
+	if err != nil {
+		t.Errorf("This product is different from the defined structure: %v", err)
+	}
+
+	request, err := http.NewRequest("PUT", "/products/Tegridy%20Jungle%20Bud", bytes.NewBuffer(weedJSON))
+	if err != nil {
+		t.Errorf("Error performing request: %v", err)
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	response := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(UpdateProduct)
+	handler.ServeHTTP(response, request)
+
+	t.Run("Response Code", func(t *testing.T) {
+		got := response.Code
+		want := http.StatusOK
+		if got != want {
+			t.Errorf("Unexpected response code ... got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("Updated date is different from initialized date", func(t *testing.T) {
+		var singleProduct model.Product
+		err := json.NewDecoder(response.Body).Decode(&singleProduct)
+		if err != nil {
+			t.Errorf("Error parsing response data into product structure: %v", err)
+		}
+
+		if singleProduct.Name != "Tegridy Jungle Bud" && singleProduct.Updated != "2022-22-22 00:12:01" {
+			t.Error("Response data does not have correct information")
+		}
+	})
+}
+
+func TestDeleteExistingProduct(t *testing.T) {
+	request, err := http.NewRequest("DELETE", "/products/Organic%20House%20Blend", nil)
+	if err != nil {
+		t.Errorf("Error performing request: %v", err)
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	response := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(DeleteProduct)
+	handler.ServeHTTP(response, request)
+
+	t.Run("Response Code", func(t *testing.T) {
+		got := response.Code
+		want := http.StatusOK
+		if got != want {
+			t.Errorf("Unexpected response code ... got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("Header", func(t *testing.T) {
+		got := response.Header().Get("Content-Type")
+		want := "application/json"
+		if got != want {
+			t.Errorf("Invalid response header ... got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("'Organic House Blend' not part of existing products", func(t *testing.T) {
+		var productCatalog model.Product
+		// cannot be used in a subsequent subtest because response data gets consumed
+		err := json.NewDecoder(response.Body).Decode(&model.Products)
+		if err != nil {
+			t.Errorf("Error parsing response data into product structure: %v", err)
+		}
+
+		if productCatalog.Name == "Organic House Blend" {
+			t.Error("Product was not deleted from catalog")
+		}
+	})
+
 }
